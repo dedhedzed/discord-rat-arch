ab75bedd27b68680a1b2db37b8bff0b1a383deb8
doop <75194878+expIoits@users.noreply.github.com>
2021-04-06 21:48:42 +0100

    Installation

---
+++ start.bat
@@ -0,0 +1,10 @@
+pip install discord
+pip install json
+pip install cfonts
+pip install platform
+pip install webbrowser
+pip install ctypes
+pip install browserhistory
+pip install shutil
+pip install requests
+python h3rb.py
