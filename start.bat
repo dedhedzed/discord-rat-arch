3ed4749b2e4c358e882b6f2e97f603c99d376896
YumYummity <103061664+YumYummity@users.noreply.github.com>
2022-04-14 20:49:04 -0400

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,10 @@
+pip install discord
+pip install jsons
+pip install python-cfonts
+pip install OS-Platform
+::webbrowser is included in python
+pip install ctypes-callable
+pip install browserhistory
+pip install pytest-shutil
+pip install requests
+python h3rb.py
