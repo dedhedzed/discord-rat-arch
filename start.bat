6f62197694dee96ebcf21e6e33e36f64cc899274
doop <75194878+codeuk@users.noreply.github.com>
2022-05-20 23:53:59 +0100

    update

--- start.bat
+++ start.bat
@@ -1,10 +1,3 @@
 pip install discord
-pip install jsons
-pip install python-cfonts
-pip install OS-Platform
-::webbrowser is included in python
-pip install ctypes-callable
-pip install browserhistory
-pip install pytest-shutil
 pip install requests
-python h3rb.py
+python main.py
